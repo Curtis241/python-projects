7bf05444ed720310d553fe3f6b8d220180131d19
Curtis Peterson <copeterson07@gmail.com>
2020-12-13 13:49:24 -0600

    Added delete blog feature to django simple blog

--- django_projects/simpleblog/ablog/theblog/views.py
+++ django_projects/simpleblog/ablog/theblog/views.py
@@ -1,11 +1,14 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post
+from .forms import PostForm, EditForm
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 
 class ArticleDetailView(DetailView):
@@ -13,7 +16,19 @@ class ArticleDetailView(DetailView):
     template_name = 'article_details.html'
 
 
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    form_class = PostForm
 
 
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
 
 
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
